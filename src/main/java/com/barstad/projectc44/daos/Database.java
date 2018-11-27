d9fb415b49e6e2cb9e353dd77240c4e352db555c
oystein barstad <oebars@gmail.com>
2018-11-27 10:21:20 +0100

    Some more cleanup and standardization

--- src/main/java/com/barstad/projectc44/daos/Database.java
+++
@@ -1,9 +0,0 @@
-package com.barstad.projectc44.daos;
-
-import com.barstad.projectc44.Models.UserProfile;
-
-public interface Database {
-    public void openConnection();
-    public UserProfile saveUserProfile(UserProfile userProfile);
-    public void closeConnection();
-}
